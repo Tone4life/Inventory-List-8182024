3421d0abb2324c8384fa06789c8b85a49962108a
Tone4life <Toney@exceptional1movers.com>
2024-08-21 19:22:44 -0400

    modified:   node_modules/express/lib/application.js

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cookieParser());
+
+const csrfProtection = csrf({ cookie: true });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+app.get('/', csrfProtection, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/submit_form', csrfProtection, (req, res) => {
+    res.send('Form data received');
+});
+
+app.use((err, req, res, next) => {
+    if (err.code === 'EBADCSRFTOKEN') {
+        res.status(403);
+        res.send('Invalid CSRF token');
+    } else {
+        next(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cookieParser());
+
+const csrfProtection = csrf({ cookie: true });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+app.get('/', csrfProtection, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/submit_form', csrfProtection, (req, res) => {
+    res.send('Form data received');
+});
+
+app.use((err, req, res, next) => {
+    if (err.code === 'EBADCSRFTOKEN') {
+        res.status(403);
+        res.send('Invalid CSRF token');
+    } else {
+        next(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
